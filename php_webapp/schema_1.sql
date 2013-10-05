926b6a249e9cc8c1b3a49a5e1825beb38249b599
Jason Long <jason@long.name>
2013-10-05 11:53:27 -0400

    schema: upgrade from v1 to v2
    adds Game_Definition table

---
+++ php_webapp/schema_1.sql
@@ -0,0 +1,23 @@
+CREATE TABLE game_definition (
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	tournament INTEGER NOT NULL,
+	name VARCHAR(200) NOT NULL,
+	FOREIGN KEY (tournament) REFERENCES tournament (id)
+	);
+
+INSERT INTO game_definition (tournament,name)
+	SELECT DISTINCT tournament,game
+	FROM contest
+	WHERE game IS NOT NULL
+	ORDER BY tournament,game;
+
+ALTER TABLE contest CHANGE game game_name VARCHAR(200);
+ALTER TABLE contest ADD COLUMN game INTEGER AFTER game_name;
+ALTER TABLE contest ADD FOREIGN KEY (game) REFERENCES game_definition (id);
+UPDATE contest c SET game=(SELECT id FROM game_definition g WHERE g.name=c.game_name AND g.tournament=c.tournament)
+	WHERE game_name IS NOT NULL;
+ALTER TABLE contest DROP COLUMN game_name;
+
+ALTER TABLE tournament ADD COLUMN default_game INTEGER;
+
+UPDATE master SET version=2;
